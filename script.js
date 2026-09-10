12de50641a4c0d8501c598b5403e14b572604fba
xKgolxl <homervan2802@gmail.com>
2026-09-11 00:59:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -22,21 +22,21 @@ const contents = {
         </div>
       </div>
       <div class="right-column" id="resume-text">
-        <p>I am a beginner Data Analyst, full of enthusiasm and eager to develop in the fields of Data Analysis, Big Data, and Machine Learning. Despite lacking professional experience, I possess strong analytical thinking and a genuine passion for data and information. I quickly learn new tools and technologies, constantly improving my skills through self-study, online courses, and practical projects.</p>
+        <p>lack of professional experience, but I possess strong analytical thinking and a genuine passion for data and information. I quickly learn new tools and technologies, constantly improving my skills through self-study, online courses, and practical projects.</p>
 
-        <p>My main goal is to become part of a team of professionals where I can not only apply my knowledge but also learn from experienced colleagues by solving business-scale problems. I am open to new challenges and ready to contribute to the development of projects that bring real value to the company.</p>
+        <p>Become part of a team of professionals where I can not only apply my knowledge but also learn from experienced colleagues by solving business-scale problems. I am open to new challenges and ready to contribute to the development of projects that bring real value to the company.</p>
 
-        <p>My key skills include:</p>
+        <p>My key skills:</p>
 
         <ul>
-          <li>Confident English at the B2–C1 level — necessary for reading technical documentation, communicating with colleagues, and learning new tools.</li>
+          <li>English B2–C1 level — reading technical documentation, communicating with colleagues, and learning new tools.</li>
           <li>The ability to maintain focus and productivity when working with large volumes of data and tight deadlines.</li>
-          <li>The ability to quickly master new technologies, tools, and data analysis methods, which is especially important in the fast-paced field of analytics.</li>
+          <li>The ability to quickly master new technologies, tools, and data analysis methods, which is especially important in the fast-paced world.</li>
         </ul>
       </div>
     </div>
   `,
-  projects: "<p>Here are some cool projects I've worked on.</p>",
+  projects: "<p>projects</p>",
 
   skills: `
     <div class="skills-container-extended">
